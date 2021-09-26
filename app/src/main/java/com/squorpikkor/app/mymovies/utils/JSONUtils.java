e737669a606b9ae3f4f3972838ccf10d006b094c
VadimSquorpikkor <uniqueehub1024>
2021-09-26 07:46:36 +0300

    урок 79 1. Отзывы и трейлеры

--- app/src/main/java/com/squorpikkor/app/mymovies/utils/JSONUtils.java
+++ app/src/main/java/com/squorpikkor/app/mymovies/utils/JSONUtils.java
@@ -1,12 +1,20 @@
 package com.squorpikkor.app.mymovies.utils;
 
 import com.squorpikkor.app.mymovies.data.Movie;
+import com.squorpikkor.app.mymovies.data.Review;
+import com.squorpikkor.app.mymovies.data.Trailer;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+
+import static com.squorpikkor.app.mymovies.utils.NetworkUtils.BASE_YOUTUBE_URL;
+import static com.squorpikkor.app.mymovies.utils.NetworkUtils.KEY_AUTHOR;
+import static com.squorpikkor.app.mymovies.utils.NetworkUtils.KEY_CONTENT;
+import static com.squorpikkor.app.mymovies.utils.NetworkUtils.KEY_KEY_OF_VIDEO;
+import static com.squorpikkor.app.mymovies.utils.NetworkUtils.KEY_NAME;
 
 public class JSONUtils {
 
@@ -24,6 +32,42 @@ public class JSONUtils {
     private static final String BASE_POSTER_URL = "https://image.tmdb.org/t/p/";
     private static final String SMALL_POSTER_SIZE = "w185";
     private static final String BIG_POSTER_SIZE = "w780";
+
+    public static ArrayList<Review> getReviewsFromJSON(JSONObject jsonObject) {
+        ArrayList<Review> result = new ArrayList<>();
+        if (jsonObject == null) return result;
+        try {
+            JSONArray jsonArray = jsonObject.getJSONArray(KEY_RESULTS);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObjectReview = jsonArray.getJSONObject(i);
+                String author = jsonObjectReview.getString(KEY_AUTHOR);
+                String content = jsonObjectReview.getString(KEY_CONTENT);
+                Review review = new Review(author, content);
+                result.add(review);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static ArrayList<Trailer> getTrailersFromJSON(JSONObject jsonObject) {
+        ArrayList<Trailer> result = new ArrayList<>();
+        if (jsonObject == null) return result;
+        try {
+            JSONArray jsonArray = jsonObject.getJSONArray(KEY_RESULTS);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObjectTrailer = jsonArray.getJSONObject(i);
+                String key = BASE_YOUTUBE_URL + jsonObjectTrailer.getString(KEY_KEY_OF_VIDEO);
+                String name = jsonObjectTrailer.getString(KEY_NAME);
+                Trailer trailer= new Trailer(key, name);
+                result.add(trailer);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 
     public static ArrayList<Movie> getMoviesFromJSON(JSONObject jsonObject) {
         ArrayList<Movie> result = new ArrayList<>();
--- app/src/main/java/com/squorpikkor/app/mymovies/utils/NetworkUtils.java
+++ app/src/main/java/com/squorpikkor/app/mymovies/utils/NetworkUtils.java
@@ -16,6 +16,8 @@ import java.util.concurrent.ExecutionException;
 
 public class NetworkUtils {
 
+    //api docs: https://developers.themoviedb.org/3/getting-started/introduction
+
     //Учетка: htpps://www.themoviedb.org
     //Squorpikkor
     //uniqueemovie
@@ -23,10 +25,25 @@ public class NetworkUtils {
 
     //Пример API-запроса https://api.themoviedb.org/3/movie/550?api_key=d65a76f91a0d1e2911264b29198fb636
 
+    //API-запрос фильма https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=<<api_key>>&language=en-US
+    //API-запрос отзыва https://api.themoviedb.org/3/movie/{movie_id}/reviews?api_key=<<api_key>>&language=en-US&page=1
+
     //Ключ доступа к API (v4 auth)
     private static final String API_KEY_4 = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNjVhNzZmOTFhMGQxZTI5MTEyNjRiMjkxOThmYjYzNiIsInN1YiI6IjYwNGQwOWMyMWQ1Mzg2MDA1MzEwMTg4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.m9dxsnrQuxOBPpBxpLwqlVirvZpsNjedEfMuvHZnyDE";
 
+    //Для видео
+    public static final String KEY_KEY_OF_VIDEO = "key";
+    public static final String KEY_NAME = "name";
+
+    //Для отзывов
+    public static final String KEY_AUTHOR = "author";
+    public static final String KEY_CONTENT = "content";
+
+    //Вся информация о фильме
     private static final String BASE_URL = "https://api.themoviedb.org/3/discover/movie";
+    private static final String BASE_URL_VIDEOS = "https://api.themoviedb.org/3/movie/%s/videos";
+    private static final String BASE_URL_REVIEW = "https://api.themoviedb.org/3/movie/%s/reviews";
+    public static final String BASE_YOUTUBE_URL = "https://www.youtube.com/watch?v=";
 
 
     private static final String PARAMS_API_KEY = "api_key";
@@ -42,6 +59,32 @@ public class NetworkUtils {
 
     public static final int POPULARITY = 0;
     public static final int TOP_RATED = 1;
+
+    private static URL buildURLToVideos(int id) {
+        Uri uri = Uri.parse(String.format(BASE_URL_VIDEOS, id)).buildUpon()
+                .appendQueryParameter(PARAMS_API_KEY, API_KEY_4)
+                .appendQueryParameter(PARAMS_LANGUAGE, LANGUAGE_VALUE)
+                .build();
+        try {
+            return new URL(uri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static URL buildURLToReview(int id) {
+        Uri uri = Uri.parse(String.format(BASE_URL_REVIEW, id)).buildUpon()
+                .appendQueryParameter(PARAMS_API_KEY, API_KEY_4)
+                .appendQueryParameter(PARAMS_LANGUAGE, LANGUAGE_VALUE)
+                .build();
+        try {
+            return new URL(uri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     private static URL buildURL(int sortBy, int page) {
         URL result = null;
@@ -68,6 +111,32 @@ public class NetworkUtils {
     public static JSONObject getJSONFromNetwork(int sortBy, int page) {
         JSONObject result = null;
         URL url = buildURL(sortBy, page);
+        try {
+            result = new JSONLoadTask().execute(url).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static JSONObject getJSONForVideos(int id) {
+        JSONObject result = null;
+        URL url = buildURLToVideos(id);
+        try {
+            result = new JSONLoadTask().execute(url).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static JSONObject getJSONForReview(int id) {
+        JSONObject result = null;
+        URL url = buildURLToReview(id);
         try {
             result = new JSONLoadTask().execute(url).get();
         } catch (ExecutionException e) {
